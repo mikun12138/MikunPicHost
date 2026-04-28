960bca31bbe0416b778d640121574290b57146be
mikun_12138 <mikun.12138@gmail.com>
2026-04-28 17:15:47 +0800

    add install scripts

--- scripts/install_mikunpichost_linux.sh
+++ scripts/install_mikunpichost_linux.sh
@@ -6,24 +6,25 @@ JAR_NAME="MikunPicHost.jar"
 
 SERVICE_FILE_URL="https://raw.githubusercontent.com/mikun12138/MikunPicHost/refs/heads/master/scripts/mikun-pic-host.service"
 
-SERVICE_FILE="mikun-pic-host.service"
+SERVICE_FILE_TEMP="mikun-pic-host.service"
 
 SERVICE_NAME="mikun-pic-host"
 REAL_USER=${SUDO_USER:-$USER}
 REAL_GROUP=$(id -gn $REAL_USER)
-JAVA_PATH=$(which java)
 
 mkdir -p "$WORKING_DIR"
 curl -L "$JAR_URL" -o "$WORKING_DIR/$JAR_NAME"
-curl -L "$SERVICE_FILE_URL" -o "$(pwd)/$SERVICE_FILE"
+curl -L "$SERVICE_FILE_URL" -o "$(pwd)/$SERVICE_FILE_TEMP"
 
 sed -e "s|{{USER}}|$REAL_USER|g" \
     -e "s|{{GROUP}}|$REAL_GROUP|g" \
     -e "s|{{WORKING_DIR}}|$WORKING_DIR|g" \
-    -e "s|{{JAVA_PATH}}|$JAVA_PATH|g" \
     -e "s|{{JAR_NAME}}|$JAR_NAME|g" \
     -e "s|{{SERVICE_NAME}}|$SERVICE_NAME|g" \
-    "$SERVICE_FILE" > "/etc/systemd/system/$SERVICE_NAME.service"
+    "$SERVICE_FILE_TEMP" > "/etc/systemd/system/$SERVICE_NAME.service"
+
+chown -R "$REAL_USER:$REAL_USER" "$WORKING_DIR"
+sudo rm "$SERVICE_FILE_TEMP"
 
 systemctl daemon-reload
 systemctl enable "$SERVICE_NAME"
